fbd7320dbe5d2a6236f40fb6b8836920392cf171
Lumo4k <luanmorales321@gmail.com>
2023-05-30 22:34:52 -0300

    Revert "Finalizado exercícios do módulo 3 da Coursera USP sobre if/else"
    This reverts commit 088449610f8c4743b144728696fdbf6830f9048f.

--- Coursera/mod3_FizzBuzzParcialPt1.py
+++
@@ -1,6 +0,0 @@
-numb = int(input("Digite um número: "))
-
-if numb%3 == 0:
-    print("Fizz")
-else:
-    print(numb)
--- Coursera/mod3_par_impar.py
+++
@@ -1,6 +0,0 @@
-numb = int(input("Digite um número: "))
-
-if numb%2 == 0:
-    print("Esse número é par!")
-else:
-    print("Esse número é ímpar!")
